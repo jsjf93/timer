f899d3fd449c1fd1c6858fce684c9d3657f526dc
Josh Foster <jsjf93@hotmail.co.uk>
2020-08-04 13:38:59 +0100

    Play a sound once timer finished

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,32 +1,39 @@
 import React, { useState, useEffect } from 'react';
 import moment from 'moment';
+import useSound from 'use-sound';
 
 import './App.scss';
 import Countdown from '../Timer/Countdown/Countdown';
 import ProgressBar from '../Timer/ProgressBar/ProgressBar';
 import Button from '../Button/Button';
 
-const tempValue = moment.duration({ minutes: 20, seconds: 0 });
+const bellSfx = require('../../sounds/bell.mp3');
+
+const tempValue = moment.duration({ minutes: 0, seconds: 10 });
 
 const App = () => {
   const [timer, setTimer] = useState<moment.Duration>(tempValue);
   const [isActive, setIsActive] = useState(true);
-  const percentage = Math.round((timer.asMilliseconds() / tempValue.asMilliseconds()) * 100);
+  const [playSound] = useSound(bellSfx);
 
-  const calculateTime = () => {
-    const newTimer = moment.duration(timer);
-    newTimer.subtract(1, 's');
-    newTimer.asMilliseconds() >= 0 ? setTimer(newTimer) : setIsActive(false);
-  };
+  const percentage = (timer.asMilliseconds() / tempValue.asMilliseconds()) * 100;
 
   useEffect(() => {
+    const calculateTime = () => {
+      const newTimer = moment.duration(timer);
+      newTimer.subtract(1, 's');
+      newTimer.asMilliseconds() >= 0 ? setTimer(newTimer) : setIsActive(false);
+    };
+
     let timeout: NodeJS.Timeout;
     if (isActive) {
       timeout = setTimeout(() => calculateTime(), 1000);
+    } else {
+      playSound();
     }
 
     return () => clearTimeout(timeout);
-  });
+  }, [isActive, playSound, timer]);
 
   return (
     <div className="container">
